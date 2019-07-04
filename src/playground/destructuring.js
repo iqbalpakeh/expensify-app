79f10db7888a687a20fcac996a6b856b291c4f22
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-05 03:28:41 +0700

    challenge

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,16 +1,27 @@
-const person = {
-	name: "Andrew",
-	age: 26,
-	location: {
-		city: "Medan",
-		temp: 92
+// const person = {
+// 	name: "Andrew",
+// 	age: 26,
+// 	location: {
+// 		city: "Medan",
+// 		temp: 92
+// 	}
+// };
+
+// const { name: firstName = "Anonymous", age } = person;
+// console.log(`${firstName} is ${age}.`);
+
+// const { city, temp: temprature } = person.location;
+// if (city && temprature) {
+// 	console.log(`It's ${temprature} in ${city}`);
+// }
+
+const book = {
+	title: "Ego is the Enemy",
+	author: "Ryan Holiday",
+	publisher: {
+		name: "Penguin"
 	}
 };
 
-const { name: firstName = "Anonymous", age } = person;
-console.log(`${firstName} is ${age}.`);
-
-const { city, temp: temprature } = person.location;
-if (city && temprature) {
-	console.log(`It's ${temprature} in ${city}`);
-}
+const { name: publisherName = "Self-Published" } = book.publisher;
+console.log(publisherName);
