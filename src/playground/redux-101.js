059d998ac68985822b0b7091948ea29babcd5914
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-04 12:51:25 +0800

    Add more action

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,20 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
 	switch (action.type) {
 		case "INCREMENT":
+			const incrementBy =
+				typeof action.incrementBy === "number" ? action.incrementBy : 1;
 			return {
-				count: state.count + 1
+				count: state.count + incrementBy
 			};
 		case "DECREMENT":
+			const decrementBy =
+				typeof action.decrementBy === "number" ? action.decrementBy : 1;
 			return {
-				count: state.count - 1
+				count: state.count - decrementBy
+			};
+		case "SET":
+			return {
+				count: action.count
 			};
 		case "RESET":
 			return {
@@ -24,10 +32,11 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch({
-	type: "INCREMENT"
+	type: "INCREMENT",
+	incrementBy: 5
 });
 
-unsubscribe();
+// unsubscribe();
 
 store.dispatch({
 	type: "INCREMENT"
@@ -39,4 +48,14 @@ store.dispatch({
 
 store.dispatch({
 	type: "DECREMENT"
+});
+
+store.dispatch({
+	type: "DECREMENT",
+	decrementBy: 10
+});
+
+store.dispatch({
+	type: "SET",
+	count: 101
 });
