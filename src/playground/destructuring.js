d6ce02d002bff33ec7e8a312f6c1d1f80054c167
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-08 03:49:34 +0700

    Follow andrew solution

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// OBJECT DESTRUCTURING
+//
+
 // const person = {
 // 	name: "Andrew",
 // 	age: 26,
@@ -15,13 +19,30 @@
 // 	console.log(`It's ${temprature} in ${city}`);
 // }
 
-const book = {
-	title: "Ego is the Enemy",
-	author: "Ryan Holiday",
-	publisher: {
-		name: "Penguin"
-	}
-};
+// const book = {
+// 	title: "Ego is the Enemy",
+// 	author: "Ryan Holiday",
+// 	publisher: {
+// 		name: "Penguin"
+// 	}
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = "Self-Published" } = book.publisher;
+// console.log(publisherName);
+
+//
+// ARRAY DESTRUCTURING
+//
+
+// const address = [
+// 	"1299 S Juniper Stree",
+// 	"Phildelphia",
+// 	"Pennsylvania",
+// 	"19147"
+// ];
+// const [, city, state = "New York"] = address;
+// console.log(`You are in ${city} ${state}`);
+
+const item = ["Coffee (iced)", "$2.00", "$2.50", "$2.75"];
+const [itemName, , mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
