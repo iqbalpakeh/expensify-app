059775a654a94c8e9abf6ee83c955e412ff59339
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-12 06:33:07 +0700

    Add more code

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -53,17 +53,35 @@ const setTextFilter = (text = "") => ({
 // SORT_BY_DATE
 // -------------------------------------------------
 
+const sortByDate = () => ({
+	type: "SORT_BY_DATE"
+});
+
 // -------------------------------------------------
 // SORT_BY_AMOUNT
 // -------------------------------------------------
+
+const sortByAmount = () => ({
+	type: "SORT_BY_AMOUNT"
+});
 
 // -------------------------------------------------
 // SET_START_DATE
 // -------------------------------------------------
 
+const setStartDate = (startDate = undefined) => ({
+	type: "SET_START_DATE",
+	startDate
+});
+
 // -------------------------------------------------
 // SET_END_DATE
 // -------------------------------------------------
+
+const setEndDate = endDate => ({
+	type: "SET_END_DATE",
+	endDate
+});
 
 // -------------------------------------------------
 // EXPENSE REDUCER
@@ -75,8 +93,10 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 	switch (action.type) {
 		case "ADD_EXPENSE":
 			return [...state, action.expense];
+
 		case "REMOVE_EXPENSE":
 			return state.filter(({ id }) => id !== action.id);
+
 		case "EDIT_EXPENSE":
 			return state.map(expense => {
 				if (expense.id === action.id) {
@@ -85,6 +105,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 					return expense;
 				}
 			});
+
 		default:
 			return state;
 	}
@@ -108,6 +129,31 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 				...state,
 				text: action.text
 			};
+
+		case "SORT_BY_AMOUNT":
+			return {
+				...state,
+				sortBy: "amount"
+			};
+
+		case "SORT_BY_DATE":
+			return {
+				...state,
+				sortBy: "date"
+			};
+
+		case "SET_START_DATE":
+			return {
+				...state,
+				startDate: action.startDate
+			};
+
+		case "SET_END_DATE":
+			return {
+				...state,
+				endDate: action.endDate
+			};
+
 		default:
 			return state;
 	}
@@ -132,20 +178,27 @@ store.subscribe(() => {
 // MAIN()
 // -------------------------------------------------
 
-const expenseOne = store.dispatch(
-	addExpense({ description: "rent", amount: 100 })
-);
+// const expenseOne = store.dispatch(
+// 	addExpense({ description: "rent", amount: 100 })
+// );
 
-const expenseTwo = store.dispatch(
-	addExpense({ description: "coffee", amount: 300 })
-);
+// const expenseTwo = store.dispatch(
+// 	addExpense({ description: "coffee", amount: 300 })
+// );
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter("rent"));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter("rent"));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(1250));
 
 // -------------------------------------------------
 // DUMMY DATA
