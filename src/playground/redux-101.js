a813005ba241e7f6e28a7536835690a680444a46
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-15 06:16:50 +0700

    Improve code readability

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -26,7 +26,7 @@ const resetCount = () => ({
 // --------------------------
 // REDUCER
 //
-// 1. Reducre are pure functions.
+// 1. Reducers are pure functions.
 // 2. Never change state or action
 // --------------------------
 
@@ -38,24 +38,32 @@ const countReducer = (state = { count: 0 }, action) => {
 			return {
 				count: state.count + incrementBy
 			};
+
 		case "DECREMENT":
 			const decrementBy =
 				typeof action.decrementBy === "number" ? action.decrementBy : 1;
 			return {
 				count: state.count - decrementBy
 			};
+
 		case "SET":
 			return {
 				count: action.count
 			};
+
 		case "RESET":
 			return {
 				count: 0
 			};
+
 		default:
 			return state;
 	}
 };
+
+// --------------------------
+// STORE CREATION
+// --------------------------
 
 const store = createStore(countReducer);
 
@@ -63,43 +71,20 @@ const unsubscribe = store.subscribe(() => {
 	console.log(store.getState());
 });
 
-// store.dispatch({
-// 	type: "INCREMENT",
-// 	incrementBy: 5
-// });
+// --------------------------
+// MAIN
+// --------------------------
 
 store.dispatch(incrementCount({ incrementBy: 5 }));
 
 // unsubscribe();
 
-// store.dispatch({
-// 	type: "INCREMENT"
-// });
-
 store.dispatch(incrementCount());
-
-// store.dispatch({
-// 	type: "RESET"
-// });
 
 store.dispatch(resetCount());
 
-// store.dispatch({
-// 	type: "DECREMENT"
-// });
-
 store.dispatch(decrementCount());
 
-// store.dispatch({
-// 	type: "DECREMENT",
-// 	decrementBy: 10
-// });
-
 store.dispatch(decrementCount({ decrementBy: 10 }));
-
-// store.dispatch({
-// 	type: "SET",
-// 	count: 101
-// });
 
 store.dispatch(setCount({ count: 101 }));
