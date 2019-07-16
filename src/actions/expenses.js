ae69ea06662d15ddf5a77088a0f81c0675f93f8c
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-16 12:02:51 +0800

    Start to merge redux to react project

---
+++ src/actions/expenses.js
@@ -0,0 +1,40 @@
+import uuid from "uuid";
+
+// -------------------------------------------------
+// ACTION ADD_EXPENSE
+// -------------------------------------------------
+
+export const addExpense = ({
+	description = "",
+	note = "",
+	amount = 0,
+	createdAt = 0
+} = {}) => ({
+	type: "ADD_EXPENSE",
+	expense: {
+		id: uuid(),
+		description,
+		note,
+		amount,
+		createdAt
+	}
+});
+
+// -------------------------------------------------
+// ACTION REMOVE_EXPENSE
+// -------------------------------------------------
+
+export const removeExpense = ({ id } = {}) => ({
+	type: "REMOVE_EXPENSE",
+	id
+});
+
+// -------------------------------------------------
+// ACTION EDIT_EXPENSE
+// -------------------------------------------------
+
+export const editExpense = (id, updates) => ({
+	type: "EDIT_EXPENSE",
+	id,
+	updates
+});
