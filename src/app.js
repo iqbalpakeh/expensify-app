9b5eb4730ae0a9e03bca9f6c2ba1ce04c732c65c
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-16 19:38:42 +0800

    Conect react with redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
@@ -10,13 +11,23 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
-console.log(store.getState());
+// console.log(store.getState());
 
 store.dispatch(addExpense({ description: "Water bill" }));
 store.dispatch(addExpense({ description: "Gas bill" }));
 store.dispatch(setTextFilter("water"));
 
 const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
+// console.log(getVisibleExpenses(state.expenses, state.filters));
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+setTimeout(() => {
+	store.dispatch(setTextFilter("rent"));
+}, 3000);
+
+const jsx = (
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import ExpenseList from "./ExpenseList";
 
 const ExpenseDashboardPage = () => (
-	<div>This is from my dahsboard component</div>
+	<div>
+		<ExpenseList />
+	</div>
 );
 
 export default ExpenseDashboardPage;
