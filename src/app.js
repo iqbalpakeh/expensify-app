39a9469cdeeae720d021bf81d3a86ce65ff7c8da
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-17 12:30:41 +0800

    better code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
