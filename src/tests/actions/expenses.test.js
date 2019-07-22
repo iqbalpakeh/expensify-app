dc89dbcc7859d4a6aa760087c579ea538184c60f
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-22 08:59:00 +0700

    Add more test

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -18,3 +18,37 @@ test("should setup edit expense action object", () => {
 		}
 	});
 });
+
+test("should setup add expense action object with provided values", () => {
+	const expenseData = {
+		description: "Rent",
+		amount: 109500,
+		createdAt: 1000,
+		note: "This was last month rent"
+	};
+	const action = addExpense(expenseData);
+	expect(action).toEqual({
+		type: "ADD_EXPENSE",
+		expense: {
+			...expenseData,
+			id: expect.any(String)
+		}
+	});
+});
+
+test("should setup add expense action object with default values", () => {
+	const expenseDefaultData = {
+		description: "",
+		note: "",
+		amount: 0,
+		createdAt: 0
+	};
+	const action = addExpense();
+	expect(action).toEqual({
+		type: "ADD_EXPENSE",
+		expense: {
+			...expenseDefaultData,
+			id: expect.any(String)
+		}
+	});
+});
