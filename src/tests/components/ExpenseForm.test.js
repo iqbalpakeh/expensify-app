622094cd7583e4d97f7b0c1372d6fa0a981617d2
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-28 06:01:32 +0700

    Add more test

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import moment from "moment";
 import ExpenseForm from "../../components/ExpenseForm";
 import expenses from "../fixtures/expenses";
 
@@ -87,4 +88,18 @@ test("should call onSubmit prop for valid form submission", () => {
 		createdAt: expenses[0].createdAt,
 		note: expenses[0].note
 	});
+});
+
+test("should set new data on date change", () => {
+	const now = moment();
+	const wrapper = shallow(<ExpenseForm />);
+	wrapper.find("SingleDatePicker").prop("onDateChange")(now);
+	expect(wrapper.state("createdAt")).toEqual(now);
+});
+
+test("should set calendar focus on change", () => {
+	const focused = true;
+	const wrapper = shallow(<ExpenseForm />);
+	wrapper.find("SingleDatePicker").prop("onFocusChange")({ focused });
+	expect(wrapper.state("calendarFocused")).toEqual(focused);
 });
