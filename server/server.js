814b954ce549ce4dd7ad8f095c19d5d7ce7219e2
Progrema Studio <iqbalpakeh@gmail.com>
2019-07-31 09:56:14 +0700

    Prepare for heroku deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+
 const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -19,6 +21,6 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log("Server is up!");
 });
