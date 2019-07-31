f90f4cccf203728225959606655f9c4a806fbd7d
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-01 04:30:55 +0700

    Change test data for second test

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -1,14 +1,14 @@
 import getTotalExpenses from "../../selectors/expenses-total";
 import expenses from "../fixtures/expenses";
 
-test("should return 0 if no expense", () => {
+test("should return 0 if no expenses", () => {
 	const total = getTotalExpenses([]);
 	expect(total).toBe(0);
 });
 
-test("should correctly add up a single expenses", () => {
-	const total = getTotalExpenses([expenses[1]]);
-	expect(total).toBe(expenses[1].amount);
+test("should correctly add up a single expense", () => {
+	const total = getTotalExpenses([expenses[0]]);
+	expect(total).toBe(195);
 });
 
 test("should correctly add up multiple expenses", () => {
