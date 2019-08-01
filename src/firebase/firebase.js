98e7e075a86e7cc4792327e8af61ebaf925b96f5
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-02 04:25:05 +0700

    Integrate firebase to project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDksOWUm-iKOXNxMOQd_IL5rQZm4Uj_4lo",
+	authDomain: "expensify-app-ec7bb.firebaseapp.com",
+	databaseURL: "https://expensify-app-ec7bb.firebaseio.com",
+	projectId: "expensify-app-ec7bb",
+	storageBucket: "",
+	messagingSenderId: "490139563764",
+	appId: "1:490139563764:web:0743900e03417900"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// dummy testing the database connection
+firebase
+	.database()
+	.ref()
+	.set({
+		name: "Iqbal Pakeh"
+	});
