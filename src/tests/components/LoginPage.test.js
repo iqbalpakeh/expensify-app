ceb391abd2f0df8bd5bde3d2d420c340839fbe2b
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-07 12:40:40 +0800

    Add startLogin function to firebase

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import LoginPage from "../../components/LoginPage";
+import { LoginPage } from "../../components/LoginPage";
 
 test("should render LoginPage correctly", () => {
 	const wrapper = shallow(<LoginPage />);
