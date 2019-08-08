0c26b69ff2d433f096f2fd7787b49b2cb6ec94e0
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-09 04:48:12 +0700

    Implement user specific expense path

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -23,7 +23,8 @@ export const addExpense = expense => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-	return dispatch => {
+	return (dispatch, getState) => {
+		const uid = getState().auth.uid;
 		const {
 			description = "",
 			note = "",
@@ -32,7 +33,7 @@ export const startAddExpense = (expenseData = {}) => {
 		} = expenseData;
 		const expense = { description, note, amount, createdAt };
 		return database
-			.ref("expenses")
+			.ref(`users/${uid}/expenses`)
 			.push(expense)
 			.then(ref => {
 				dispatch(
@@ -96,9 +97,10 @@ export const setExpenses = expenses => ({
 });
 
 export const startSetExpenses = () => {
-	return dispatch => {
+	return (dispatch, getState) => {
+		const uid = getState().auth.uid;
 		return database
-			.ref("expenses")
+			.ref(`users/${uid}/expenses`)
 			.once("value")
 			.then(snapshot => {
 				const expenses = [];
