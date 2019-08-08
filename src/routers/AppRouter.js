211e0cd900dcf112fb8bf49ad52a6baf9310bb60
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-08 18:02:44 +0800

    Remove HelpPage from project

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,7 +5,6 @@ import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import NotFoundPage from "../components/NotFoundPage";
-import HelpPage from "../components/HelpPage";
 import LoginPage from "../components/LoginPage";
 import PrivateRoute from "./PrivateRoute";
 
@@ -19,7 +18,6 @@ const AppRouter = () => (
 				<PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
 				<PrivateRoute path="/create" component={AddExpensePage} />
 				<PrivateRoute path="/edit/:id" component={EditExpensePage} />
-				<Route path="/help" component={HelpPage} />
 				<Route component={NotFoundPage} />
 			</Switch>
 		</div>
