c39693da7e2eab44306820ac863ca369341cb61f
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-10 08:15:37 +0700

    Add ExpenseList style

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,7 +4,12 @@ import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 
 export const ExpenseList = props => (
-	<div>
+	<div className="content-container">
+		<div className="list-header">
+			<div className="show-for-mobile">Expenses</div>
+			<div className="show-for-desktop">Expense</div>
+			<div className="show-for-desktop">Amount</div>
+		</div>
 		<h1>Expense List</h1>
 		{props.expenses.length === 0 ? (
 			<p>No expenses</p>
